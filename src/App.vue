a39f74803aea925f00c67d0a1e196140608e1042
ricardocruz <ricardo.cruz@swau.edu>
2024-03-07 17:14:03 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <body>
+        <HeaderVue />
+        <RouterView />
+        <FooterVue />
+    </body>
+</template>
+
+<script setup lang="ts">
+import HeaderVue from './components/Header.vue'
+import FooterVue from './components/Footer.vue'
+</script>
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import '@unocss/reset/tailwind.css'
+import 'virtual:uno.css'
+import './style.css'
+import App from './App.vue'
+import router from './router'
+const app = createApp(App)
+app.use(router)
+
+app.mount('#app')
